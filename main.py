64eb792ad0fb9472800a4b2d2380497683bd7aaf
wozniak04 <53666197+wozniak04@users.noreply.github.com>
2024-11-24 12:49:15 +0000

    discordowy bot wysyla wiadomosci

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+import asyncio
 from selenium.webdriver.chrome.options import Options
 from przejscie_po_zakladkach import przechodzenie
-
+from wysylaniedodiscorda import send_phone
 
 # Set options for ChromeDriver*
 options = Options()
@@ -17,23 +17,28 @@ options.add_argument("--disable-extensions");
 options.add_argument("--disable-gpu");
 options.add_argument("--disable-dev-shm-usage");
 
-# Initialize Chrome driver with options
-driver = webdriver.Chrome(options=options)
-driver.get("https://www.olx.pl/elektronika/telefony/smartfony-telefony-komorkowe/iphone/q-iphone")
+# Funkcja główna, która będzie wywoływała asynchronicznie send_phone
+async def main():
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.olx.pl/elektronika/telefony/smartfony-telefony-komorkowe/iphone/q-iphone")
 
-zakladka = driver.find_element(By.CLASS_NAME, "css-4mw0p4").find_elements(By.TAG_NAME, "a")
-liczba_zakladek = int(zakladka[3].text)
-nr_zakladki = 1
-dobreceny = []
+    zakladka = driver.find_element(By.CLASS_NAME, "css-4mw0p4").find_elements(By.TAG_NAME, "a")
+    liczba_zakladek = int(zakladka[3].text)
+    nr_zakladki = 1
+    dobreceny = []
 
-while nr_zakladki <= liczba_zakladek:
-    wynik = przechodzenie(driver, zakladka, nr_zakladki, liczba_zakladek)
-    liczba_zakladek = wynik[0]
-    dobreceny.extend(wynik[1])
-    zakladka = wynik[2]
-    nr_zakladki += 1
-    if nr_zakladki == (liczba_zakladek + 1):
-        nr_zakladki = 1
-    print(len(dobreceny))
+    while nr_zakladki <= liczba_zakladek:
+        wynik = przechodzenie(driver, zakladka, nr_zakladki, liczba_zakladek)
+        liczba_zakladek = wynik[0]
+        dobreceny.extend(wynik[1])
+        zakladka = wynik[2]
+        nr_zakladki += 1
+        if nr_zakladki == (liczba_zakladek + 1):
+            nr_zakladki = 1
+        await send_phone(dobreceny)  # Wywołanie asynchroniczne
+        dobreceny=[]
 
-driver.quit()
+    driver.quit()
+
+# Uruchomienie głównej funkcji
+asyncio.run(main())
--- wysylaniedodiscorda.py
+++ wysylaniedodiscorda.py
@@ -1,3 +1,35 @@
 import discord
+import os
 
-client = discord.Client()
+
+intents = discord.Intents.default()
+intents.messages = True  # Jeśli bot ma odczytywać i wysyłać wiadomości
+Token = os.getenv("Token")
+# Prawidłowe utworzenie klienta z intencjami
+client = discord.Client(intents=intents)
+kanal=int(os.getenv("IDchannel"))
+
+# Wydarzenie: bot gotowy do działania
+@client.event
+async def on_ready():
+    print(f'Bot zalogowany jako {client.user}')
+
+
+
+async def send_phone(telefony):
+    for i in telefony:
+        print(i)
+        # try:
+            
+        #     channel = await client.fetch_channel(kanal)
+        #     await channel.send("Hello world") 
+        # except discord.NotFound:
+        #     print("Kanał o podanym ID nie istnieje.")
+        # except discord.Forbidden:
+        #     print("Bot nie ma wystarczających uprawnień do dostępu do kanału.")
+        # except discord.HTTPException as e:
+        #     print(f"Nie udało się wysłać wiadomości: {e}")
+
+
+
+client.run(Token)
